709df3ea89ad3609dd764019c6a9fc3fb2f2c95f
Jong Min Yu <uok0201@gmail.com>
2021-02-05 18:56:05 +0900

    Initial commit

---
+++ src/main/java/com/myintroduce/MyintroduceApplication.java
@@ -0,0 +1,13 @@
+package com.myintroduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyintroduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyintroduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myintroduce/MyintroduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myintroduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyintroduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
