1ed7b6ee384ca20bcfbe3e528e4f69b64a9de220
wellbell <uok02018496@naver.com>
2021-02-09 01:35:28 +0900

    cors 설정 수정

--- src/main/java/com/myintroduce/config/CorsConfig.java
+++ src/main/java/com/myintroduce/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); // e.g. http://domain1.com
+        config.addAllowedOriginPattern("*"); // e.g. http://domain1.com
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
--- src/main/java/com/myintroduce/config/SecurityConfig.java
+++ src/main/java/com/myintroduce/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .addFilter(corsConfig.corsFilter())
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
