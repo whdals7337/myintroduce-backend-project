ab866262ec77d7712e1b07d27c41563f1b34e1e6
Jong Min Yu <uok0201@gmail.com>
2021-02-16 00:31:35 +0900

    scouter java agent 를 위해서 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,7 +35,9 @@ echo "> $JAR_NAME 실행"
 java
 
 nohup java -jar \
-    -javaagent:/home/ec2-user/scouter/agent.java/scouter.agent.jar -Dobj_name=MY-INTRODUCE-WAS-8081 -DScouter.config=/home/ec2-user/scouter/agent.java/conf/myintroduce8081.conf \
+    -javaagent:/home/ec2-user/scouter/agent.java/scouter.agent.jar \
+    -Dobj_name=MY-INTRODUCE-WAS-8081 \
+    -DScouter.config=/home/ec2-user/scouter/agent.java/conf/myintroduce8081.conf \
     -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-ops.yml \
     -Dspring.profiles.active=real1 \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
@@ -75,7 +77,9 @@ do
       echo "> $JAR_NAME 실행"
 
       nohup java -jar \
-          -javaagent:/home/ec2-user/scouter/agent.java/scouter.agent.jar -Dobj_name=MY-INTRODUCE-WAS-8081 -DScouter.config=/home/ec2-user/scouter/agent.java/conf/myintroduce8081.conf \
+          -javaagent:/home/ec2-user/scouter/agent.java/scouter.agent.jar \
+          -Dobj_name=MY-INTRODUCE-WAS-8082 \
+          -DScouter.config=/home/ec2-user/scouter/agent.java/conf/myintroduce8082.conf \
           -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-ops.yml \
           -Dspring.profiles.active=real2 \
           $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
