0ecac1973c61c98536257b1808cf944e6e52e95e
wellbell <uok02018496@naver.com>
2021-03-08 18:26:22 +0900

    [test] DateUtil getTodayByCustomFormat 메서드 Calendar -> LocalDateTime 수정 및 테스트 코드 작성

--- src/main/java/com/myintroduce/utill/DateUtil.java
+++ src/main/java/com/myintroduce/utill/DateUtil.java
@@ -1,14 +1,12 @@
 package com.myintroduce.utill;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
     // 오늘의 날짜를 원하는 포맷으로 만들어서 리턴
     public static String getTodayByCustomFormat(String format) {
-        SimpleDateFormat fm = new SimpleDateFormat(format);
-        Calendar cal = Calendar.getInstance();
-        return fm.format(cal.getTime());
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
     }
 }
---
+++ src/test/java/com/myintroduce/utill/DateUtilTest.java
@@ -0,0 +1,29 @@
+package com.myintroduce.utill;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DateUtilTest {
+
+    @Test
+    public void getTodayByCustomFormat_test() {
+        // given
+
+        // when
+        String formatToday = DateUtil.getTodayByCustomFormat("yyyy-MM-dd");
+
+        // then
+        String today = "";
+        int month = LocalDateTime.now().getMonthValue();
+        int date = LocalDateTime.now().getDayOfMonth();
+        today += LocalDateTime.now().getYear();
+        today += "-";
+        today += month < 10 ? "0"+month : month;
+        today += "-";
+        today += date < 10 ? "0"+date : date;
+        assertThat(formatToday).isEqualTo(today);
+    }
+}
