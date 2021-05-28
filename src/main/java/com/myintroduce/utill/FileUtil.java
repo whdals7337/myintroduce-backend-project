d3148e2b511c0eb7e0b1b34232592f625d3b4453
wellbell <uok02018496@naver.com>
2021-05-28 18:06:40 +0900

    [refactor] FileUtil 전체 리팩토링

--- src/main/java/com/myintroduce/utill/FileUtil.java
+++ src/main/java/com/myintroduce/utill/FileUtil.java
@@ -11,32 +11,37 @@ import java.time.format.DateTimeFormatter;
 
 public class FileUtil {
 
-    // 파일 이름이 기준 length 보다 길경우 잘라서 리턴
+    // 파일 이름이 기준 length 보다 길경우 잘라서 리턴 메서드
     public static String cutFileName (String fileName, int length) {
-        if(fileName.length() <= length) return fileName;
+        if(fileName.length() <= length) {
+            return fileName;
+        }
 
         String extension = fileName.substring(fileName.lastIndexOf("."));
         return fileName.substring(0, length - extension.length()) + extension;
     }
 
-    // 랜덤 파일 이름 리턴
+    // 랜덤 파일 이름 리턴 메서드
     public static String getRandomFileName(String fileName) {
-        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         String extension = fileName.substring(fileName.lastIndexOf("."));
         return RandomStringUtils.randomAlphabetic(5)+ "_" + RandomStringUtils.randomNumeric(5) + "_" +
                 LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + extension;
     }
     
-    // dirPath 에 디렉토리가 없는경우 생성 메서드
+    // 디렉토리가 없는 경우 생성 메서드
     public static void createDir(String dirPath) {
         File file = new File(dirPath);
-        if(!file.exists()) file.mkdir();
+        if(!file.exists()) {
+            file.mkdir();
+        }
     }
     
     // 파일이 존재하는 경우 삭제 메서드
     public static void deleteFile(String filePath) {
         File file = new File(filePath);
-        if(file.exists()) file.delete();
+        if(file.exists()) {
+             file.delete();
+        }
     }
 
     public static FileInfo getFileInfo(String originalName, String domain,
