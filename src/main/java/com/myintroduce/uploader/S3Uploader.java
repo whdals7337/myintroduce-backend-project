0adcebd2f225b074f0407475aa67e933d9f4d384
wellbell <uok02018496@naver.com>
2021-06-10 23:12:32 +0900

    [fix] S3Uploader Profile 설정 제거

--- src/main/java/com/myintroduce/uploader/S3Uploader.java
+++ src/main/java/com/myintroduce/uploader/S3Uploader.java
@@ -6,7 +6,6 @@ import com.myintroduce.utill.FileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -20,7 +19,6 @@ import java.util.Optional;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-@Profile("ops")
 public class S3Uploader implements Uploader {
 
     private final AmazonS3Client amazonS3Client;
